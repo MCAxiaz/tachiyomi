9046cdb2c22cbdbd1d99246fedfafc529dc5dda5
MCAxiaz <mcaxiaz@gmail.com>
2020-06-23 17:00:08 -0700

    minor cleanup

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderPresenter.kt
@@ -17,7 +17,6 @@ import eu.kanade.tachiyomi.source.SourceManager
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.ui.base.presenter.BasePresenter
 import eu.kanade.tachiyomi.ui.reader.loader.ChapterLoader
-import eu.kanade.tachiyomi.ui.reader.loader.DownloadPageLoader
 import eu.kanade.tachiyomi.ui.reader.model.ReaderChapter
 import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
 import eu.kanade.tachiyomi.ui.reader.model.ViewerChapters
@@ -386,7 +385,7 @@ class ReaderPresenter(
     }
 
     /**
-     * Called when a chapter changed from [fromChapter] to [toChapter]. It updates [fromChapter]
+     * Called when a chapter changed from [fromChapter]. It updates [fromChapter]
      * on the database.
      */
     private fun onChapterChanged(fromChapter: ReaderChapter) {
